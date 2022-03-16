8fbe9ef363800d8ffc6bfd832a53b4be102e6901
exile2003 <31631692+exile2003@users.noreply.github.com>
2022-03-16 23:28:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+      <>
+      <h1>Количество нажатий {count}</h1>
+          <button onClick = {setCount(count - 1)}>Уменьшить</button>
+          <button onClick = {setCount(count + 1)}>Увеличить</button>
+      </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
